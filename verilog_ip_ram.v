0b20bad143d90292451de3033911d4eca6a8049a
ChangYW1996 <28745859+ChangYW1996@users.noreply.github.com>
2019-10-04 16:48:56 +0800

    Update verilog_ip_ram.v

--- verilog_ip_ram.v
+++ verilog_ip_ram.v
@@ -3,10 +3,10 @@ module verilog_ip_ram
 input clk_50M,
 input   RST_N,
 
-output reg [4:0] address,  
+output reg [7:0] address,  
 output reg [7:0] wrdata,    
 output [7:0] rddata,    
-output reg [5:0] time_cnt,   
+output reg [7:0] time_cnt,   
 output  wren,					 
 output  rden					 
 );
@@ -15,21 +15,36 @@ always@(posedge clk_50M or negedge RST_N)
 begin
 	if(!RST_N)
 		 time_cnt<=0;
-	else if(time_cnt==6'd63)
+	else if(time_cnt==7'd63)
 		time_cnt<=0;
 	else time_cnt<=time_cnt+1'b1;
 end
-	assign wren=(time_cnt>=6'd0&&time_cnt<=6'd31);
-	assign rden=(time_cnt>=6'd32&&time_cnt<=6'd63);
+assign wren=(time_cnt>=7'd0&&time_cnt<=7'd31);
+assign rden=(time_cnt>=7'd32&&time_cnt<=7'd63);
 
 always@(negedge clk_50M or  negedge RST_N )
 begin
 	if(!RST_N)
 		wrdata<=0;
 	else 
-		wrdata<=time_cnt;
+		begin
+			wrdata<=time_cnt;
+		end
 end
-
+always@(negedge clk_50M or  negedge RST_N )
+begin
+	if(!RST_N)
+		address<=0;
+	else 
+		if(time_cnt<=7'd31)
+			begin
+				address<=time_cnt;
+			end
+		else
+			begin
+				address<=time_cnt-7'd32;
+			end
+end
 
 RAM	RAM_init (
 	.address ( address ),   
@@ -39,6 +54,5 @@ RAM	RAM_init (
 	.wren ( wren ),    
 	.q ( rddata )     
 	);
-	
 	
 endmodule
